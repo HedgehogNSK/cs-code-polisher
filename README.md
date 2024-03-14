33772c7cc3ba629afb92682773668580547363f7
Vladislav Ivashnev <hedgensk@gmail.com>
2024-03-14 09:24:39 +0700

    ฺีRework format usings methods Bug fixes for format using: 1. Additive spaces at the begining prevent from removing unnecessary using. 2. If unnecessary using were in the same line with active usings, it wasn't able to be removed. Update README

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # C# Code Polisher
 
-It's an extension for **VS Code** under **Roslyn LSP** manage. The extension provides ability to sort and remove unnecessary usings both in separate C# lang files and in the whole workspace. Also it provides ability to clean unnecessary whitespaces.
+It's an extension for **VS Code** under **Roslyn LSP** manage. **IT WILL NOT WORK FOR OMNISHARP AND OTHER LSP**
 
-**IT WON'T WORK FOR OMNISHARP AND OTHER LSP**
+The extension provides functions for:
+* Removing unnecessary usings in a separate .cs file and in a workspace as well. If you wish to remove unnecessary usings in a workspace, you have to setup project at first. Please read the requirements section for additional information.
+* Soritng usings by name in a file/workspace.
+* Cleaning unnecessary whitespaces in a file/workspace.
 
 ## Features
 
-Extension allows to do the chores for C# usings via VS code command palette. Keep in a mind that removing of unnecessary usings can be executed successfully only when LSP finishes its checks. So you need to wait until LSP to light up files in workspace before using the extension. Clean whitespace function can be execute instantly.
+Extension allows to do the chores for usings in C# files via VS code command palette. Keep in mind that removing of unnecessary usings can be executed successfully only when LSP finishes its file analyses. So you should wait until LSP lights up files in a workspace before execute the usings cleaning function. The whitespace cleaning function can be execute instantly.
 
 1. Shift + Command + P (Mac) / Ctrl + Shift + P (Windows/Linux)
 2. Insert 'CodePolisher'
@@ -16,7 +19,7 @@ Extension allows to do the chores for C# usings via VS code command palette. Kee
 
 ## Requirements
 
-Extension works for VS Code editor (1.85.0 and higher) that uses Roslyn LSP. This extension will not work for OmniSharp LSP or any other due to differences in structure of diagnostics data that different LSPs collects. 
+Extension works for VS Code editor (1.85.0 and higher) that uses Roslyn LSP. This extension **will not work for OmniSharp LSP** or any other due to differences in structure of diagnostics data that different LSPs collects. 
 
 If you wish to have ability to clean unnecessary usings for closed files of a workspace, then you have to add line to `.vscode/settings.json` of the project or of a user:
 
@@ -32,6 +35,11 @@ This extension contributes the following settings:
 * `cscodepolisher.excludePathFromChecking`: [GlobPattern](https://code.visualstudio.com/docs/editor/glob-patterns) - paths that need to be excluded from the check.
 
 You can change them in Settings menu for certain project or for user
+
+### 0.0.4
+
+* Rework of usings removing and sorting processes due to bugs.
+
 ### 0.0.3
 
 * Add function that removes unnecessary whitespace for current file and for workspace as well
--- src/usingsPolisher.ts
+++ src/usingsPolisher.ts
@@ -5,8 +5,8 @@ export enum Procedure {
     SortImorts = 1 << 0,
     RemoveUnnececcaryImports = 1 << 1,
 }
-
-const DoesLineHasUsing = (text: string): number => text.search(/using\s+[.\w]+;/);
+const usingRegExp = new RegExp(/using\s+[.\w]+;/gm);
+const usingBlockRegEx = new RegExp(/\s*(using\s+[.\w]+;\s*)+/gm);
 
 export async function formatAllDocuments(this: any) {
     let procedure = this as Procedure;
@@ -35,44 +35,18 @@ export async function formatUsingsWrapper(this: any, editor: vscode.TextEditor, 
 
 export async function formatUsings(document: vscode.TextDocument, procedure: Procedure) {
     var options = tools.getFormatOptions();
-    let usingsaMap = getUsingLines(document);
-    let usingsBlocks = getUsingsBlocks(document);
-    let edit = new vscode.WorkspaceEdit();
-
-    let hasUnnecessaryUsings: boolean = false;
-    if (((procedure | Procedure.RemoveUnnececcaryImports) == procedure)) {
-        let unnecessaryUsingsBlocks = getUnnecessaryUsingsRange(document);
-        if (unnecessaryUsingsBlocks.some(v => v !== null && typeof v !== "undefined")) {
-            hasUnnecessaryUsings = true;
-            usingsaMap = usingsaMap.filter(usingLine => !unnecessaryUsingsBlocks.some(range => range.contains(usingLine.range)));
-        }
-    }
-
     try {
-        let lines = Array.from(usingsaMap.values());
-        let isSorted: boolean = true;
+
+        if (((procedure | Procedure.RemoveUnnececcaryImports) == procedure)) {
+            let edit = removeUnnecessaryImports(document);
+            await vscode.workspace.applyEdit(edit);
+        }
+
         if (((procedure | Procedure.SortImorts) == procedure)) {
-            isSorted = lines.every((value, index, array) =>
-                index === 0 || value.text.localeCompare(array[index - 1].text) != -1);
-            lines = lines.sort((a, b) => a.text.localeCompare(b.text));
+            let edit = sortUsings(document);
+            await vscode.workspace.applyEdit(edit);
         }
-        //If file doesn't have any unnecessary usings and all usings are sorted
-        //then break the fixing process to prevent uninvolved files to be marked as dirty 
-        if (!hasUnnecessaryUsings && isSorted)
-            return;
-        //End of line
-        const eol = document.eol === vscode.EndOfLine.LF ? '\n' : '\r\n';
-        for (let id = usingsBlocks.length - 1; id != -1; --id) {
-            let currentRange = usingsBlocks[id];
-            let currentLines = lines.filter(line => currentRange.contains(line.range));
-            let position = currentRange.end;
-            for (let j = 0; j != currentLines.length; ++j) {
-                let textLine = currentLines[j];
-                edit.insert(document.uri, position, textLine.text + eol);
-            }
-            edit.delete(document.uri, usingsBlocks[id]);
-        }
-        vscode.workspace.applyEdit(edit);
+
         if (options.saveOnEdit)
             document.save();
     }
@@ -99,51 +73,97 @@ export interface IDiagnosticArgs {
     document: vscode.TextDocument;
     changesCount: number;
 }
-/**
- * Returns TextLines that contents "usings"
- * @param document {@link vscode.TextDocument}
- * @returns using lines {@link Array<vs.TextLine> }.
- */
 
-export function getUsingLines(document: vscode.TextDocument): Array<vscode.TextLine> {
-    let array: Array<vscode.TextLine> = new Array();
-    for (let id = 0; id != document.lineCount; ++id) {
-        let line = document.lineAt(id);
-        let hasUsing = DoesLineHasUsing(line.text);
-        if (!line.isEmptyOrWhitespace && hasUsing == 0) {
-            array.push(line);
+export function sortUsings(document: vscode.TextDocument): vscode.WorkspaceEdit {
+    const edit = new vscode.WorkspaceEdit();
+    const text = document.getText();
+    const eol = document.eol === vscode.EndOfLine.CRLF ? '\r\n' : '\n';
+    const eol2 = eol + eol;
+    const compareFunction = (x: string, y: string) => x.localeCompare(y);
+
+    let blockMatch;
+    while ((blockMatch = usingBlockRegEx.exec(text)) !== null) {
+        const startPosition = document.positionAt(blockMatch.index);
+        const endPosition = document.positionAt(blockMatch.index + blockMatch[0].length);
+        const range = new vscode.Range(startPosition, endPosition);
+
+        let usingExp;
+        let usingsArray: string[] = [];
+        let blockString = blockMatch[0];
+
+        while ((usingExp = usingRegExp.exec(blockString)) !== null) {
+            insertInSortedArray(usingsArray, usingExp[0], compareFunction);
         }
+        let result = usingsArray.join(eol) + eol2;
+        if (blockMatch.index != 0)
+            result = eol2 + result;
+        edit.replace(document.uri, range, result);
     }
-    return array;
+    return edit;
 }
-/**
- * Return Array of blocks that contain usings 
- * If 2 usings that are splited by end of the line or by whitespace, they goes into same block
- * If 2 usings that are splited by language constructions as namespaces, commas, etc, they goes into different blocks
- * @param document {@link vscode.TextDocument}
- * @returns restricted blocks {@link  Array<vs.Range>} .
- */
-export function getUsingsBlocks(document: vscode.TextDocument): Array<vscode.Range> {
-    let array: Array<vscode.Range> = new Array();
-    let range;
-    let evenOneUsing: boolean = false;
-    for (let id = 0; id != document.lineCount; ++id) {
-        let line = document.lineAt(id);
-        let hasUsing = DoesLineHasUsing(line.text);
-        if (line.isEmptyOrWhitespace || hasUsing == 0) {
-            if (range != undefined)
-                range = line.range.union(range);
-            else
-                range = line.range;
-
-            if (!evenOneUsing && hasUsing == 0)
-                evenOneUsing = true;
-
-        } else if (evenOneUsing && range != undefined) {
-            array.push(range);
-            range = undefined;
-            evenOneUsing = false;
-        }
+function insertInSortedArray<T>(array: T[], element: T, compareFunction: (param1: T, param2: T) => number): void {
+    let index = array.findIndex(_item => compareFunction(_item, element) > 0);
+    if (index === -1) {
+        index = array.length;
     }
-    return array;
+    array.splice(index, 0, element);
+}
+
+export function removeUnnecessaryImports(document: vscode.TextDocument): vscode.WorkspaceEdit {
+
+    let edit = new vscode.WorkspaceEdit();
+    let unnecessaryUsingsBlocks = getUnnecessaryUsingsRange(document);
+
+    if (unnecessaryUsingsBlocks.length == 0) return edit;
+    const zeroPosition = new vscode.Position(0, 0);
+    const eofPosition = document.lineAt(document.lineCount - 1).range.end;
+    const regex = /\s+/g;
+    let firstEmptySpaceRegExp = /\s+/;
+    for (let id = 0; id != unnecessaryUsingsBlocks.length; ++id) {
+
+        let range = unnecessaryUsingsBlocks[id];
+
+        let searchPoint = id == 0 ? zeroPosition : unnecessaryUsingsBlocks[id - 1].end;
+        let searchRange = new vscode.Range(searchPoint, range.start);
+        let text = document.getText(searchRange);
+
+        let match: RegExpExecArray | null;
+        let lastIndex: number = 0;
+        let lastMatch: RegExpExecArray | null = null;
+        while ((match = regex.exec(text)) != null) {
+            lastIndex = match.index;
+            lastMatch = match;
+        }
+
+        if (lastMatch !== null) {
+            let offset = document.offsetAt(searchPoint) + lastIndex;
+            let usingOffset = document.offsetAt(range.start);
+            let matchStartPos = document.positionAt(offset);
+            if (offset + lastMatch[0].length == usingOffset)
+                range = range.with(matchStartPos);
+        }
+
+        searchPoint = id == unnecessaryUsingsBlocks.length - 1 ? eofPosition : unnecessaryUsingsBlocks[id + 1].start;
+        searchRange = new vscode.Range(range.end, searchPoint);
+        text = document.getText(searchRange);
+
+        if (!range.start.isEqual(zeroPosition))
+            firstEmptySpaceRegExp = /\s+(?=$)/m;
+
+        match = firstEmptySpaceRegExp.exec(text);
+        if (match !== null && match.index == 0) {
+            let offset = document.offsetAt(range.end);
+            let matchPosition = document.positionAt(offset + match[0].length);
+            range = range.with(undefined, matchPosition);
+        }
+
+        unnecessaryUsingsBlocks[id] = range;
+    }
+
+    for (let id = unnecessaryUsingsBlocks.length - 1; id != -1; --id) {
+
+        edit.delete(document.uri, unnecessaryUsingsBlocks[id]);
+    }
+
+    return edit;
 }
